afb66df128c4e48b5a18a6c5b5305e0349acf37f
chiara327 <chiara.grossele@studenti.unipd.it>
2025-05-10 22:18:54 +0200

    Correzione Test primi 50

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -18,204 +18,213 @@ public class RomanPrinterTest {
         }
 
     @Test
-    public void testIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class,
-         () -> RomanPrinter.print(0));
-        assertThrows(IllegalArgumentException.class,
-         () -> RomanPrinter.print(1001));
-        assertThrows(IllegalArgumentException.class,
-         () -> RomanPrinter.print(-1));
+        public void testIllegalArgumentException() {
+            assertThrows(IllegalArgumentException.class,
+            () -> RomanPrinter.print(0));
+            assertThrows(IllegalArgumentException.class,
+            () -> RomanPrinter.print(1001));
+            assertThrows(IllegalArgumentException.class,
+            () -> RomanPrinter.print(-1));
+        }
+
+    @Test
+    public void ShouldPrintI(){
+        int numero =1;
+        String ascii=RomanPrinter.print(numero);
+        assertEquals("  _____ \n"+
+                     " |_   _|\n"+
+                     "   | |  \n"+
+                     "   | |  \n"+
+                     "  _| |_ \n"+
+                     " |_____|\n", ascii);
     }
 
     @Test
-	public void ShouldPrintI() {
-        int numero = 1;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals(" _____ " + "\n" 
-                   + " |_ _| " + "\n"
-                   + "  | |  " + "\n" 
-                   + "  | |  " + "\n" 
-                   + " _| |_ " + "\n" 
-                   + "|_____|", ascii);
-	}
+    public void ShouldPrintII(){
+        int numero =2;
+        String ascii=RomanPrinter.print(numero);
+        assertEquals("  _____   _____ \n"+
+                     " |_   _| |_   _|\n"+
+                     "   | |     | |  \n"+
+                     "   | |     | |  \n"+
+                     "  _| |_   _| |_ \n"+
+                     " |_____| |_____|\n", ascii);
+        
+    }
 
     @Test
-	public void ShouldPrintII() {
-        int numero = 2;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals(" _____  _____ " + "\n" 
-                   + " |_ _|  |_ _| " + "\n"
-                   + "  | |    | |  " + "\n" 
-                   + "  | |    | |  " + "\n" 
-                   + " _| |_  _| |_ " + "\n" 
-                   + "|_____||_____|", ascii);
-	}
+    public void ShouldPrintIII(){
+        int numero =3;
+        String ascii=RomanPrinter.print(numero);
+        assertEquals("  _____   _____   _____ \n"+
+                     " |_   _| |_   _| |_   _|\n"+
+                     "   | |     | |     | |  \n"+
+                     "   | |     | |     | |  \n"+
+                     "  _| |_   _| |_   _| |_ \n"+
+                     " |_____| |_____| |_____|\n", ascii);
+        
+    }
 
     @Test
-	public void ShouldPrintIII() {
-        int numero = 3;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals(" _____  _____  _____ " + "\n" 
-                   + " |_ _|  |_ _|  |_ _| " + "\n"
-                   + "  | |    | |    | |  " + "\n" 
-                   + "  | |    | |    | |  " + "\n" 
-                   + " _| |_  _| |_  _| |_ " + "\n" 
-                   + "|_____||_____||_____|", ascii);
-	}
+    public void ShouldPrintIV(){
+        int numero =4;
+        String ascii=RomanPrinter.print(numero);
+        assertEquals("  _____  __      __\n"+
+                     " |_   _| \\ \\    / /\n"+
+                     "   | |    \\ \\  / / \n"+
+                     "   | |     \\ \\/ /  \n"+
+                     "  _| |_     \\  /   \n"+
+                     " |_____|     \\/    \n", ascii);
+        
+    }
 
     @Test
-	public void ShouldPrintIV() {
-        int numero = 4;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals(" _____ __      __" + "\n"
-                    +" |_ _| \\ \\    / /" + "\n"
-                    +"  | |   \\ \\  / / " + "\n"
-                    +"  | |    \\ \\/ /  " + "\n"
-                    +" _| |_    \\  /   " + "\n"
-                    +"|_____|    \\/    " , ascii);
-	}
+    public void ShouldPrintV(){
+        int numero =5;
+        String ascii=RomanPrinter.print(numero);
+        assertEquals(" __      __\n"+
+                     " \\ \\    / /\n"+
+                     "  \\ \\  / / \n"+
+                     "   \\ \\/ /  \n"+
+                     "    \\  /   \n"+
+                     "     \\/    \n", ascii);
+        
+    }
 
     @Test
-	public void ShouldPrintV() {
-        int numero = 5;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals("__      __" + "\n"
-                    +"\\ \\    / /" + "\n"
-                    +" \\ \\  / / " + "\n"
-                    +"  \\ \\/ /  " + "\n"
-                    +"   \\  /   " + "\n"
-                    +"    \\/    " , ascii);
-	}
+    public void ShouldPrintVI(){
+        int numero =6;
+        String ascii=RomanPrinter.print(numero);
+        assertEquals(" __      __  _____ \n"+
+                     " \\ \\    / / |_   _|\n"+
+                     "  \\ \\  / /    | |  \n"+
+                     "   \\ \\/ /     | |  \n"+
+                     "    \\  /     _| |_ \n"+
+                     "     \\/     |_____|\n", ascii);
+        
+    }
 
     @Test
-	public void ShouldPrintVI() {
-        int numero = 6;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals("__      __ _____ " + "\n" 
-                   + "\\ \\    / / |_ _| " + "\n"
-                   + " \\ \\  / /   | |  " + "\n" 
-                   + "  \\ \\/ /    | |  " + "\n" 
-                   + "   \\  /    _| |_ " + "\n" 
-                   + "    \\/    |_____|", ascii);
-	}
+    public void ShouldPrintVII(){
+        int numero =7;
+        String ascii=RomanPrinter.print(numero);
+        assertEquals(" __      __  _____   _____ \n"+
+                     " \\ \\    / / |_   _| |_   _|\n"+
+                     "  \\ \\  / /    | |     | |  \n"+
+                     "   \\ \\/ /     | |     | |  \n"+
+                     "    \\  /     _| |_   _| |_ \n"+
+                     "     \\/     |_____| |_____|\n", ascii);
+        
+    }
 
     @Test
-	public void ShouldPrintVII() {
-        int numero = 7;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals("__      __ _____  _____ " + "\n" 
-                   + "\\ \\    / / |_ _|  |_ _| " + "\n"
-                   + " \\ \\  / /   | |    | |  " + "\n" 
-                   + "  \\ \\/ /    | |    | |  " + "\n" 
-                   + "   \\  /    _| |_  _| |_ " + "\n" 
-                   + "    \\/    |_____||_____|", ascii);
-	}
+    public void ShouldPrintVIII(){
+        int numero =8;
+        String ascii=RomanPrinter.print(numero);
+        assertEquals(" __      __  _____   _____   _____ \n"+
+                     " \\ \\    / / |_   _| |_   _| |_   _|\n"+
+                     "  \\ \\  / /    | |     | |     | |  \n"+
+                     "   \\ \\/ /     | |     | |     | |  \n"+
+                     "    \\  /     _| |_   _| |_   _| |_ \n"+
+                     "     \\/     |_____| |_____| |_____|\n", ascii);
+        
+    }
 
     @Test
-	public void ShouldPrintVIII() {
-        int numero = 8;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals("__      __ _____  _____  _____ " + "\n" 
-                   + "\\ \\    / / |_ _|  |_ _|  |_ _| " + "\n"
-                   + " \\ \\  / /   | |    | |    | |  " + "\n" 
-                   + "  \\ \\/ /    | |    | |    | |  " + "\n" 
-                   + "   \\  /    _| |_  _| |_  _| |_ " + "\n" 
-                   + "    \\/    |_____||_____||_____|", ascii);
-	}
+    public void ShouldPrintIX(){
+        int numero =9;
+        String ascii=RomanPrinter.print(numero);
+        assertEquals("  _____  __   __\n"+
+                     " |_   _| \\ \\ / /\n"+
+                     "   | |    \\ V / \n"+
+                     "   | |     > <  \n"+
+                     "  _| |_   / . \\ \n"+
+                     " |_____| /_/ \\_\\\n", ascii);
+        
+    }
 
     @Test
-	public void ShouldPrintIX() {
-        int numero = 9;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals(" _____ __   __" + "\n" 
-                   + " |_ _| \\ \\ / /" + "\n"
-                   + "  | |   \\ V / " + "\n" 
-                   + "  | |    > <  " + "\n" 
-                   + " _| |_  / . \\ " + "\n" 
-                   + "|_____|/_/ \\_\\", ascii);
-	}
-
-    @Test
-	public void ShouldPrintX() {
+    public void ShouldPrintX(){
         int numero = 10;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals("__   __" + "\n" 
-                   + "\\ \\ / /" + "\n"
-                   + " \\ V / " + "\n" 
-                   + "  > <  " + "\n" 
-                   + " / . \\ " + "\n" 
-                   + "/_/ \\_\\", ascii);
-	}
+        String ascii=RomanPrinter.print(numero);
+        assertEquals(" __   __\n"+
+                     " \\ \\ / /\n"+
+                     "  \\ V / \n"+
+                     "   > <  \n"+
+                     "  / . \\ \n"+
+                     " /_/ \\_\\\n", ascii);
+        
+    }
 
-    @Test
-	public void ShouldPrintXV() {
+    @Test 
+        public void ShouldPrintXV(){
         int numero = 15;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals("__   ____      __" + "\n" 
-                   + "\\ \\ / /\\ \\    / /" + "\n"
-                   + " \\ V /  \\ \\  / / " + "\n" 
-                   + "  > <    \\ \\/ /  " + "\n" 
-                   + " / . \\    \\  /   " + "\n" 
-                   + "/_/ \\_\\    \\/    ", ascii);
-	}
+        String ascii =  RomanPrinter.print(numero);
+        assertEquals(" __   __ __      __\n"+
+                     " \\ \\ / / \\ \\    / /\n"+
+                     "  \\ V /   \\ \\  / / \n"+
+                     "   > <     \\ \\/ /  \n"+
+                     "  / . \\     \\  /   \n"+
+                     " /_/ \\_\\     \\/    \n", ascii);
+    }
 
-    @Test
-	public void ShouldPrintXIX() {
+    @Test 
+        public void ShouldPrintXIX(){
         int numero = 19;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals("__   __ _____ __   __" + "\n" 
-                   + "\\ \\ / / |_ _| \\ \\ / /" + "\n"
-                   + " \\ V /   | |   \\ V / " + "\n" 
-                   + "  > <    | |    > <  " + "\n" 
-                   + " / . \\  _| |_  / . \\ " + "\n" 
-                   + "/_/ \\_\\|_____|/_/ \\_\\", ascii);
-	}
+        String ascii =  RomanPrinter.print(numero);
+        assertEquals(" __   __  _____  __   __\n"+
+                     " \\ \\ / / |_   _| \\ \\ / /\n"+
+                     "  \\ V /    | |    \\ V / \n"+
+                     "   > <     | |     > <  \n"+
+                     "  / . \\   _| |_   / . \\ \n"+
+                     " /_/ \\_\\ |_____| /_/ \\_\\\n", ascii);
+    }
 
-    @Test
-	public void ShouldPrintXX() {
+    @Test 
+        public void ShouldPrintXX(){
         int numero = 20;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals("__   ____   __" + "\n" 
-                   + "\\ \\ / /\\ \\ / /" + "\n"
-                   + " \\ V /  \\ V / " + "\n" 
-                   + "  > <    > <  " + "\n" 
-                   + " / . \\  / . \\ " + "\n" 
-                   + "/_/ \\_\\/_/ \\_\\", ascii);
-	}
+        String ascii =  RomanPrinter.print(numero);
+        assertEquals(" __   __ __   __\n"+
+                     " \\ \\ / / \\ \\ / /\n"+
+                     "  \\ V /   \\ V / \n"+
+                     "   > <     > <  \n"+
+                     "  / . \\   / . \\ \n"+
+                     " /_/ \\_\\ /_/ \\_\\\n", ascii);
+    }
 
     @Test
-	public void ShouldPrintXXX() {
-        int numero = 30;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals("__   ____   ____   __" + "\n" 
-                   + "\\ \\ / /\\ \\ / /\\ \\ / /" + "\n"
-                   + " \\ V /  \\ V /  \\ V / " + "\n" 
-                   + "  > <    > <    > <  " + "\n" 
-                   + " / . \\  / . \\  / . \\ " + "\n" 
-                   + "/_/ \\_\\/_/ \\_\\/_/ \\_\\", ascii);
-	}
-
+        public void ShouldPrintXXX(){
+            int numero = 30;
+            String ascii =  RomanPrinter.print(numero);
+            assertEquals(" __   __ __   __ __   __\n"+
+                         " \\ \\ / / \\ \\ / / \\ \\ / /\n"+
+                         "  \\ V /   \\ V /   \\ V / \n"+
+                         "   > <     > <     > <  \n"+
+                         "  / . \\   / . \\   / . \\ \n"+
+                         " /_/ \\_\\ /_/ \\_\\ /_/ \\_\\\n", ascii);
+    }
+    
     @Test
-    public void ShouldPrintXL() {
+    public void ShouldPrintXL(){
         int numero = 40;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals("__   __ _      " + "\n" 
-                   + "\\ \\ / /| |     " + "\n"
-                   + " \\ V / | |     " + "\n" 
-                   + "  > <  | |     " + "\n" 
-                   + " / . \\ | |____ " + "\n" 
-                   + "/_/ \\_\\|______|", ascii);
-	}
+        String ascii =  RomanPrinter.print(numero);
+        assertEquals(" __   __  _      \n"+
+                     " \\ \\ / / | |     \n"+
+                     "  \\ V /  | |     \n"+
+                     "   > <   | |     \n"+
+                     "  / . \\  | |____ \n"+
+                     " /_/ \\_\\ |______|\n", ascii);
+    }
 
     @Test
-    public void ShouldPrintL() {
+    public void ShouldPrintL(){
         int numero = 50;
-        String ascii = RomanPrinter.print(numero);
-        assertEquals(" _      " + "\n" 
-                   + "| |     " + "\n"
-                   + "| |     " + "\n" 
-                   + "| |     " + "\n" 
-                   + "| |____ " + "\n" 
-                   + "|______|", ascii);
-	}
+        String ascii =  RomanPrinter.print(numero);
+        assertEquals("  _      \n"+
+                     " | |     \n"+
+                     " | |     \n"+
+                     " | |     \n"+
+                     " | |____ \n"+
+                     " |______|\n", ascii);
+    }
 }
