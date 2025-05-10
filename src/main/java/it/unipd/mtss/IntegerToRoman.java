023da5cb86561ac6c6249fa1e239eda786f13f3b
Giuliano <giuliano.banchieri@studenti.unipd.it>
2025-05-10 17:34:49 +0200

    Implementazione primi 3 interi close #6

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -2,3 +2,28 @@
 // Chiara Grossele 2101063
 // Giuliano Banchieri 2101081
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert (int number){
+
+        if (number < 1 || number > 3) {
+            throw new IllegalArgumentException(number + 
+            "non è un numero valido perché non compreso fra 1 e 3.");
+        }
+
+        String[] romanLetters = {"I"};
+        int[] values = {1};
+
+        StringBuilder romanNumber = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                number -= values[i];
+                romanNumber.append(romanLetters[i]);
+            }
+        }
+        return romanNumber.toString();
+    }
+}
