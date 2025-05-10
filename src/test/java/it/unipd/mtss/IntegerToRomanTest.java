68798bd0267acc51758bf75445055963e7f86500
chiara327 <chiara.grossele@studenti.unipd.it>
2025-05-10 17:28:00 +0200

    Caricamento Test primi 3 close #5

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -2,3 +2,42 @@
 // Chiara Grossele 2101063
 // Giuliano Banchieri 2101081
 ////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntergerToRomanTest {
+
+    @Test
+    public void testIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+         () -> IntegerToRoman.convert(0));
+        assertThrows(IllegalArgumentException.class,
+         () -> IntegerToRoman.convert(1001));
+        assertThrows(IllegalArgumentException.class,
+         () -> IntegerToRoman.convert(-1));
+    }
+    
+    @Test
+	public void ShouldConvertToI() {
+        int numero = 1;
+        String romano = IntegerToRoman.convert(numero);
+        assertEquals("I", romano);
+	}
+
+    @Test
+	public void ShouldConvertToII() {
+        int numero = 2;
+        String romano = IntegerToRoman.convert(numero);
+        assertEquals("II", romano);
+	}
+
+    @Test
+	public void ShouldConvertToIII() {
+        int numero = 3;
+        String romano = IntegerToRoman.convert(numero);
+        assertEquals("III", romano);
+	}
+}
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -2,3 +2,57 @@
 // Chiara Grossele 2101063
 // Giuliano Banchieri 2101081
 ////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RomanPrinterTest {
+
+    @Test
+    public void testIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+         () -> RomanPrinter.print(0));
+        assertThrows(IllegalArgumentException.class,
+         () -> RomanPrinter.print(1001));
+        assertThrows(IllegalArgumentException.class,
+         () -> RomanPrinter.print(-1));
+    }
+
+    @Test
+	public void ShouldPrintI() {
+        int numero = 1;
+        String ascii = RomanPrinter.print(numero);
+        assertEquals(" _____ " + "\n" 
+                   + " |_ _| " + "\n"
+                   + "  | |  " + "\n" 
+                   + "  | |  " + "\n" 
+                   + " _| |_ " + "\n" 
+                   + "|_____|", ascii);
+	}
+
+    @Test
+	public void ShouldPrintII() {
+        int numero = 2;
+        String ascii = RomanPrinter.print(numero);
+        assertEquals(" _____  _____ " + "\n" 
+                   + " |_ _|  |_ _| " + "\n"
+                   + "  | |    | |  " + "\n" 
+                   + "  | |    | |  " + "\n" 
+                   + " _| |_  _| |_ " + "\n" 
+                   + "|_____||_____|", ascii);
+	}
+
+    @Test
+	public void ShouldPrintIII() {
+        int numero = 3;
+        String ascii = RomanPrinter.print(numero);
+        assertEquals(" _____  _____  _____ " + "\n" 
+                   + " |_ _|  |_ _|  |_ _| " + "\n"
+                   + "  | |    | |    | |  " + "\n" 
+                   + "  | |    | |    | |  " + "\n" 
+                   + " _| |_  _| |_  _| |_ " + "\n" 
+                   + "|_____||_____||_____|", ascii);
+	}
+}
